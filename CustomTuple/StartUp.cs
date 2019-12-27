1d6eae700b3237dd0b3eaa3ebd5f1f44c43ccfc3
Ivailo Vasiliev <ivailo_vasiliev@hotmail.com>
2019-12-27 16:59:05 +0200

    Problem 10. Tuple

---
+++ CustomTuple/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CustomTuple
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string[] personInfo = Console.ReadLine().Split();
+            string fullName = personInfo[0] + " " + personInfo[1];
+            string town = personInfo[2];
+
+            string[] beerInfo = Console.ReadLine().Split();
+            string name = beerInfo[0];
+            int liters = int.Parse(beerInfo[1]);
+
+            string[] specialNumber = Console.ReadLine().Split();
+            int specialInt = int.Parse(specialNumber[0]);
+            double specialDouble = double.Parse(specialNumber[1]);
+
+            SpecialTuple<string, string> personTuple = new SpecialTuple<string, string>(fullName, town);
+            SpecialTuple<string, int> beerTuple = new SpecialTuple<string, int>(name, liters);
+            SpecialTuple<int, double> specialTuple = new SpecialTuple<int, double>(specialInt, specialDouble);
+
+            Console.WriteLine(personTuple);
+            Console.WriteLine(beerTuple);
+            Console.WriteLine(specialTuple);
+        }
+    }
+}
